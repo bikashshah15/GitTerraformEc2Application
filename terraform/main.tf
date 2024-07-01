a34db7dd79d74d32ebc3aa35da01a670a054a1db
Bikash Shah <bikashshah15@gmail.com>
2024-06-30 23:08:58 -0500

    security group updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,25 @@
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = "vpc-06c085395c8ac4ea8"  # Your VPC ID
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "app_server" {
   ami = "ami-01b799c439fd5516a"
